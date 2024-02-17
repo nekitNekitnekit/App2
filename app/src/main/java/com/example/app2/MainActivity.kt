adb56a0bb9bec7324ce332bf2eeded1661582194
nekitNekitnekit <klientkv0711@gmail.com>
2024-02-17 13:30:45 +0200

    Create Spacer element

--- app/src/main/java/com/example/app2/MainActivity.kt
+++ app/src/main/java/com/example/app2/MainActivity.kt
@@ -11,8 +11,10 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.size
@@ -126,6 +128,8 @@ fun App2(modifier: Modifier = Modifier) {
             textTitle = textTitle,
             textInfo = textInfo
         )
+
+        Spacer(modifier = Modifier.height(20.dp))
 
         Row (
             modifier = Modifier
