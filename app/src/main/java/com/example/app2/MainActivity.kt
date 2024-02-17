65793218b8be3b94270978803a61c6f422b67483
nekitNekitnekit <klientkv0711@gmail.com>
2024-02-17 12:37:25 +0200

    Create index for current image currentImageIndex

--- app/src/main/java/com/example/app2/MainActivity.kt
+++ app/src/main/java/com/example/app2/MainActivity.kt
@@ -64,6 +64,8 @@ fun App2(modifier: Modifier = Modifier) {
     val images = listOf(R.drawable.image1, R.drawable.image2, R.drawable.image3, R.drawable.image4,
         R.drawable.image5, R.drawable.image6, R.drawable.image7, R.drawable.image8,)
 
+    var currentImageIndex by remember { mutableStateOf(0) }
+
     Column (
         modifier = Modifier
             .statusBarsPadding()
