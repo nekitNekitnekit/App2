0796875b79c6e0156631506a1bb22af5cbc6670c
nekitNekitnekit <klientkv0711@gmail.com>
2024-02-17 11:49:20 +0200

    Create simple template for future app

--- app/src/main/java/com/example/app2/MainActivity.kt
+++ app/src/main/java/com/example/app2/MainActivity.kt
@@ -3,12 +3,17 @@ package com.example.app2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.app2.ui.theme.App2Theme
 
@@ -39,8 +44,23 @@ fun App2Preview() {
 
 @Composable
 fun App2(modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello",
-        modifier = modifier
-    )
+    Column {
+
+        Image(painter = painterResource(R.drawable.image), contentDescription = "Image")
+
+        Text("Header 1", style = MaterialTheme.typography.h5)
+
+        Text("Header 2", style = MaterialTheme.typography.h6)
+
+        Row {
+            Button(onClick = { /*...*/ }) {
+                Text("Button 1")
+            }
+
+            Button(onClick = { /*...*/ }) {
+                Text("Button 2")
+            }
+        }
+
+    }
 }
